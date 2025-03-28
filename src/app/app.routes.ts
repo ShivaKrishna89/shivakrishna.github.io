de16ed3fbfbd56482ca24cb666fb788140e4ea22
Shiva Krishna <pallewarshiva@gmail.com>
2025-03-29 01:51:59 +0530

    add skills section and updated about section to include journey

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { ContactMeComponent } from './contact-me/contact-me.component';
 
 export const routes: Routes = [
-    {path:'home',component:HomeComponent},
-    {path:'about',component:AboutComponent},
-    {path:'projects',component:ProjectsComponent},
-    {path:'experience',component:ExperienceComponent},
-    {path:'contactMe',component:ContactMeComponent},
-    { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
