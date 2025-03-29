9c085795ea7d8ca6ee79103b71d27402f04bf2d1
Shiva Krishna <pallewarshiva@gmail.com>
2025-03-29 23:49:48 +0530

    Added footer and refactored other sections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
 export class HomeComponent {
   @ViewChild('contactForm') contactForm!: ElementRef<HTMLFormElement>;
   formData:any = {name:'', email:'', message:''};
+  isLoading:any = false;
 
 downloadFile() {
     const link = document.createElement('a');
@@ -20,10 +22,11 @@ downloadFile() {
 
 
 submitForm() {
-  console.log(this.contactForm);
-  
   if (!this.contactForm) return;
-
+  if(!this.formData.name || !this.formData.email || !this.formData.message){
+    alert('Please fill all required fields.')
+  } else{
+  this.isLoading = true;
   emailjs.sendForm(
     'service_vipa36r', 
     'template_osrrjny', 
@@ -32,6 +35,7 @@ submitForm() {
   )
   .then(
     (response:any) => {
+      this.isLoading = false;
       alert('Your message has been sent!');
       this.contactForm.nativeElement.reset(); 
     },
@@ -39,5 +43,6 @@ submitForm() {
       alert('Failed to send message. Please try again.');
     }
   );
+}
 }
 }
