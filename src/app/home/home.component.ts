da94b8d97e45b2e4d956302094cfd8bf686bac04
Shiva Krishna <pallewarshiva@gmail.com>
2025-03-29 21:29:27 +0530

    added contact me section and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
 export class HomeComponent {
+  @ViewChild('contactForm') contactForm!: ElementRef<HTMLFormElement>;
+  formData:any = {name:'', email:'', message:''};
+
 downloadFile() {
     const link = document.createElement('a');
     link.href = '/assets/resume.pdf';
     link.download = 'resume.pdf';
     link.click();
+}
+
+
+submitForm() {
+  console.log(this.contactForm);
+  
+  if (!this.contactForm) return;
+
+  emailjs.sendForm(
+    'service_vipa36r', 
+    'template_osrrjny', 
+    this.contactForm.nativeElement,
+    'i_iFK8A-4PFP4qEhk'  
+  )
+  .then(
+    (response:any) => {
+      alert('Your message has been sent!');
+      this.contactForm.nativeElement.reset(); 
+    },
+    (error:any) => {
+      alert('Failed to send message. Please try again.');
+    }
+  );
 }
 }
