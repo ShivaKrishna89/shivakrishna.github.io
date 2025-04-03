f73f3237a2cde484f594e0148022b21dc68ac0f2
Shiva Krishna <pallewarshiva@gmail.com>
2025-04-04 01:30:40 +0530

    header refactored in mobile added menu bar and adjusted stylings

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { PlatformModelService } from '../platform-model.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [NgbPopoverModule],
+  imports: [NgbPopoverModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
+    constructor(public platformModelService:PlatformModelService) {
+  
+    }
 }
